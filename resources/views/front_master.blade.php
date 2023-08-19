165f61cf83ceee02fa7fea1f1caf31c078b08068
Arvin5G <arvingardon5@gmail.com>
2023-08-19 14:43:50 +0800

    test_lang

--- resources/views/front_master.blade.php
+++ resources/views/front_master.blade.php
@@ -94,7 +94,7 @@
                         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
                             <li class="nav-item">
-                                <a href="{{ route('register') }}" class="nav-link 
+                                <a href="{{ route('dashboard') }}" class="nav-link 
                                     ">
                                     <i class="nav-icon fas fa-home"></i>
                                     <p>Dashboard</p>
