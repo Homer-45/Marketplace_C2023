7da2a03b7a9035e6ec5d6871ec7eb7406920f4e1
Arvin5G <arvingardon5@gmail.com>
2023-08-20 16:29:59 +0800

    change the login template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,48 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}" class="p-2">
+    
+    <form class="space-y-6" method="POST" action="{{ route('login') }}" class="p-2">
         @csrf
-
-        {{-- logo --}}
-        <div style="display: flex; justify-content: center; align-items:center; margin-bottom: 20px;">
-            <img src="{{ asset('dist/img/logo.png') }}" alt="Logo" style="width:200px; height:auto; overflow: hidden;">
-        </div>
-        <!-- Email Address -->
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+        {{-- Enter username --}}
         <div>
-            <x-input-label for="login" :value="__('Email/Name/Phone')" />
-            <x-text-input placeholder="Enter Email/Name/Phone" id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')" required autofocus autocomplete="username" />
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+            <input type="text" name="login" :value="old('login')" required autofocus autocomplete="username" id="login" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Username" required>
             <x-input-error :messages="$errors->get('login')" class="mt-2" />
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input placeholder="Enter Password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
+        {{-- Enter password --}}
+        <div>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
+        {{-- Remember_me Lost password --}}
+        <div class="flex items-start">
+            <div class="flex items-start">
+                <div class="flex items-center h-5">
+                    <input id="remember_me" type="checkbox" name="remember_me" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required>
+                </div>
+                <label for="remember_me" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+            </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-blue-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+            {{-- @if (Route::has('password.request'))
+            <a style="color:white;" class="underline text-sm text-blue-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
+            @endif --}}
 
-        <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a style="color:white;" class="underline text-sm text-blue-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+                <a href="{{ route('password.request') }}" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
             @endif
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
         </div>
 
-        <div class="social-icons">
-            <a href="#"  title="facebook"> 
-              <i class="fa fa-facebook-square" aria-hidden="true"></i>
-            </a>
-            <a href="#" title="twitter"> 
-              <i class="fa fa-twitter-square" aria-hidden="true"></i>
-            </a>
-            <a href="#" title="instagram">  
-              <i class="fa fa-instagram" aria-hidden="true"></i>
-            </a>
+        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+            Not registered? <a href="{{ route('register') }}" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
         </div>
     </form>
+
+    
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,11 +12,13 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css"  rel="stylesheet" />
+        
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased" >
-        <div class="bg" style="background-image: url('dist/img/bg.jpeg'); background-size: cover; background-repeat: no-repeat; padding:10px;">
-            <nav class="navbar">
+        <div class="container-fluid" style="background-image: url('dist/img/bg.jpeg'); background-size: cover; background-repeat: no-repeat;">
+            {{-- <nav class="navbar">
                 <div class="logo">
                     <a href="{{ route('login') }}"><img src="{{ asset('dist/img/logo.png') }}" alt="Logo"></a>
                 </div>
@@ -25,20 +27,51 @@
                     <li><a style="padding: 10px; color:blue; border-radius: 10px; background-color: whitesmoke;" href="{{ route('login') }}">Login</a></li>
                     <li><a href="{{ route('register') }}">Sign up</a></li>
                 </ul>
+            </nav> --}}
+            
+            <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+                    <a href="{{ route('login') }}" class="flex items-center p-2" style="background-color: rgba(255, 255, 255, 0.205); margin:0; width: 100px; height:auto; border-radius:10px;">
+                        <img src="{{ asset('dist/img/logo.png') }}" class="h-10 mr-3" alt="Marketplace Logo"/>
+                        {{-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Marketplace for on-demand Services</span> --}}
+                    </a>
+                    <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        </svg>
+                    </button>
+                    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+                        <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Explore</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign Up</a>
+                        </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
+  
             <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" >
                     {{-- <div>
                         <a href="/">
                             <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                         </a>
                     </div> --}}
-
-                    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style="background-color:rgba(245, 245, 245, 0.486); border-radius: 10px;">
+                    <div class="mt-2 w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
                         {{ $slot }}
                     </div>
-
-                    
+                    {{-- <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style="background-color:rgba(245, 245, 245, 0.486); border-radius: 10px;">
+                        
+                    </div> --}}
             </div>
         </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
     </body>
 </html>
